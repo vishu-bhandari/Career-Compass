554e09be5887307a185eba432be02723f9cb8e58
vishu bhandari <deevishu1234@gmail.com>
2024-05-15 22:01:03 +0530

    icons added

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,31 @@
+import { Navbar, TextInput } from "flowbite-react";
+import { Link } from "react-router-dom";
+import React from "react";
+import { AiOutlineSearch } from 'react-icons'
+
+export default function Header() {
+  return (
+    <>
+      <Navbar className=" border-b-2 ">
+        <Link
+          to="/"
+          className=" self-center whitespace-nowrap text text-sm sm:text-xl font-semibold dark:text-white  "
+        >
+          <span className="px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white ">
+            Career
+          </span>
+          Compass
+        </Link>
+        <form >
+        <TextInput
+            type="text"
+            placeholder="Search..."
+            rightIcon={}
+
+        />
+
+        </form>
+      </Navbar>
+    </>
+  );
+}
