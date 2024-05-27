781e26e4ca747fa8610d816090dc4efeb4e5ffed
vishu bhandari <deevishu1234@gmail.com>
2024-05-27 23:47:21 +0530

    created and added the footer component

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,15 +1,16 @@
 import { Footer } from "flowbite-react";
 import React from "react";
 import { Link } from "react-router-dom";
+import { BsFacebook , BsInstagram , BsTwitter , BsGithub , BsMeta , BsDribbble  } from 'react-icons/bs'
 
 
 export default function FooterCom() {
   return (
     <>
       <Footer container className=" border border-t-8 border-teal-500 ">
-        <div className="">
-          <div className="">
-            <div className="">
+        <div className="w-full max-w-7xl mx-auto ">
+          <div className="grid w-full justify-between sm:flex md:grid-cols-1">
+            <div className="mt-5 ">
               <Link
                 to="/"
                 className=" self-center whitespace-nowrap text text-lg sm:text-xl font-semibold dark:text-white  "
@@ -20,16 +21,68 @@ export default function FooterCom() {
                 Compass
               </Link>
             </div>
-            <div className="grid grid-cols-2 gap-3 sm:mt-4 sm:grid-cols-3 sm:gap-6  ">
+            <div className="grid grid-cols-2 gap-8 mt-4 sm:grid-cols-3 sm:gap-6  ">
+            <div>
+
                 <Footer.Title title="About"></Footer.Title>
                 <Footer.LinkGroup col>
                     <Footer.Link href="https://www.100jsprojects.com"
                     target="_blank" rel="noopener noreferrer">
                     Educational Resources
                     </Footer.Link>
+                    <Footer.Link href="/about"
+                    target="_blank" rel="noopener noreferrer">
+                    Career Compass Blogs
+                    </Footer.Link>
                 </Footer.LinkGroup>
             </div>
+            <div>
+
+                <Footer.Title title="Follow Us"></Footer.Title>
+                <Footer.LinkGroup col>
+                    <Footer.Link href="https://github.com"
+                    target="_blank" rel="noopener noreferrer">
+                    GitHub
+                    </Footer.Link>
+                    <Footer.Link href="https://www.linkedin.com"
+                    target="_blank" rel="noopener noreferrer">
+                    Linked In
+                    </Footer.Link>
+                </Footer.LinkGroup>
+            </div>
+            <div>
+
+                <Footer.Title title="Legal"></Footer.Title>
+                <Footer.LinkGroup col>
+                    <Footer.Link href="#"
+                    target="_blank" rel="noopener noreferrer">
+                    Privacy Policy
+                    </Footer.Link>
+                    <Footer.Link href="#"
+                    target="_blank" rel="noopener noreferrer">
+                    Terms &amp; Conditions
+                    </Footer.Link>
+                </Footer.LinkGroup>
+            </div>
+            </div>
           </div>
+          <Footer.Divider/>
+            <div className="w-full sm:flex sm:items-center  sm:justify-between ">
+                <Footer.Copyright href="#" by="Career Compass" year={new Date().getFullYear()}>
+
+                </Footer.Copyright>
+                <div className="flex gap-6 sm:mt-0 mt-4 sm:justify-center">
+                    <Footer.Icon href="#" icon={BsFacebook}/>
+                    <Footer.Icon href="#" icon={BsInstagram}/>
+                    <Footer.Icon href="#" icon={BsTwitter}/>
+                    <Footer.Icon href="#" icon={BsGithub}/>
+                    <Footer.Icon href="#" icon={BsMeta}/>
+                    <Footer.Icon href="#" icon={BsDribbble}/>
+
+                </div>
+
+            </div>
+          
         </div>
       </Footer>
     </>
