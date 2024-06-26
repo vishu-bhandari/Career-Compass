e54efb17cb7483db45a0c3fa683ecc7c378a117b
vishu bhandari <deevishu1234@gmail.com>
2024-06-27 00:12:55 +0530

    sign in  upi route created

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -48,15 +48,19 @@ export const signin = async (req, res, next) => {
     if (!validPassword) {
       return next(errorHandler(400, "Invalid password"));
     }
-    const token = jwt.sign({
-      id: validUser._id,
-
-
-    },process.env.JWT_SECRET);
-    const {password:pass,...rest}=validUser._doc;
-    res.status(200).cookie('access_token',token, {
-      httpOnly:true
-    }).json(rest);
+    const token = jwt.sign(
+      {
+        id: validUser._id,
+      },
+      process.env.JWT_SECRET
+    );
+    const { password: pass, ...rest } = validUser._doc;
+    res
+      .status(200)
+      .cookie("access_token", token, {
+        httpOnly: true,
+      })
+      .json(rest);
   } catch (error) {
     next(error);
   }
