544db5953ee07d17e4daa09e7070052685e8641d
vishu bhandari <deevishu1234@gmail.com>
2024-06-26 21:49:15 +0530

    password verification in signin page

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -31,10 +31,23 @@ export const signup = async (req, res , next ) => {
 };
 
 export const signin=async(req,res,next)=>{
-  const {username , password}=req.body;
+  const {email , password}=req.body;
 
-  if(!username || !password || username==='' || password===''){
-    
+  if(!email || !password || email==='' || password===''){
+    next(errorHandler(400,'All fields are required'));
+    return;
+  }try {
+    const validUser=await User.findOne({email});
+    if(!validUser){
+      next(errorHandler(404, 'User not found'));
+      return;
+    }
+    const validPassword=bcryptjs.compareSync(password,validUser.password);
+    if(!validPassword){
+      next(errorHandler(400, 'Invalid password'));
+    }
+  } catch (error) {
+    next(error);
   }
 
 }
