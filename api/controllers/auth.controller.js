8420bee75ae6fd2270fa609a4319418cef74ea58
vishu bhandari <deevishu1234@gmail.com>
2024-06-28 16:06:32 +0530

    sign in page functionality

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -35,32 +35,28 @@ export const signin = async (req, res, next) => {
   const { email, password } = req.body;
 
   if (!email || !password || email === "" || password === "") {
-    next(errorHandler(400, "All fields are required"));
+   next(errorHandler(400, "All fields are required"));
     return;
   }
   try {
     const validUser = await User.findOne({ email });
     if (!validUser) {
-      next(errorHandler(404, "User not found"));
+       next(errorHandler(404, "User not found"));
       return;
     }
     const validPassword = bcryptjs.compareSync(password, validUser.password);
     if (!validPassword) {
       return next(errorHandler(400, "Invalid password"));
     }
-    const token = jwt.sign(
-      {
-        id: validUser._id,
-      },
-      process.env.JWT_SECRET
-    );
-    const { password: pass, ...rest } = validUser._doc;
-    res
-      .status(200)
-      .cookie("access_token", token, {
-        httpOnly: true,
-      })
-      .json(rest);
+    const token = jwt.sign({
+      id: validUser._id,
+
+
+    },process.env.JWT_SECRET);
+    const {password:pass,...rest}=validUser._doc;
+    res.status(200).cookie('access_token',token, {
+      httpOnly:true
+    }).json(rest);
   } catch (error) {
     next(error);
   }
