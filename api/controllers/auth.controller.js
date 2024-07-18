f7a08944d62757e360a496252698750744ca38ed
vishu bhandari <deevishu1234@gmail.com>
2024-07-18 16:46:37 +0530

    google auth

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -35,28 +35,48 @@ export const signin = async (req, res, next) => {
   const { email, password } = req.body;
 
   if (!email || !password || email === "" || password === "") {
-   next(errorHandler(400, "All fields are required"));
+    next(errorHandler(400, "All fields are required"));
     return;
   }
   try {
     const validUser = await User.findOne({ email });
     if (!validUser) {
-       next(errorHandler(404, "User not found"));
+      next(errorHandler(404, "User not found"));
       return;
     }
     const validPassword = bcryptjs.compareSync(password, validUser.password);
     if (!validPassword) {
       return next(errorHandler(400, "Invalid password"));
     }
-    const token = jwt.sign({
-      id: validUser._id,
+    const token = jwt.sign(
+      {
+        id: validUser._id,
+      },
+      process.env.JWT_SECRET
+    );
+    const { password: pass, ...rest } = validUser._doc;
+    res
+      .status(200)
+      .cookie("access_token", token, {
+        httpOnly: true,
+      })
+      .json(rest);
+  } catch (error) {
+    next(error);
+  }
+};
 
-
-    },process.env.JWT_SECRET);
-    const {password:pass,...rest}=validUser._doc;
-    res.status(200).cookie('access_token',token, {
-      httpOnly:true
-    }).json(rest);
+export const google = async (res, req, next) => {
+  const { email, name, googlePhotoUrl } = req.body;
+  try {
+    const user = await User.findOne({ email });
+    if (user) {
+      const token = jwt.sign({
+        id: user._id,
+      },
+      process.env.JWT_SECRET
+    );
+    }
   } catch (error) {
     next(error);
   }
