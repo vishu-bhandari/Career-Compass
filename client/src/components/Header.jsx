d81d895d9aaa701fec5ec32a4433658914b42305
vishu bhandari <deevishu1234@gmail.com>
2024-07-19 22:13:01 +0530

    header component with user data

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,11 +1,13 @@
-import { Button, Navbar, TextInput } from "flowbite-react";
-import { Link , useLocation } from "react-router-dom";
+import { Avatar, Button, Dropdown, Navbar, TextInput } from "flowbite-react";
+import { Link, useLocation } from "react-router-dom";
 import React from "react";
-import { AiOutlineSearch } from 'react-icons/ai'
-import { FaMoon } from 'react-icons/fa'
+import { AiOutlineSearch } from "react-icons/ai";
+import { FaMoon } from "react-icons/fa";
+import { useSelector } from "react-redux";
 
 export default function Header() {
-  const path=useLocation().pathname;
+  const path = useLocation().pathname;
+  const { currentUser } = useSelector((state) => state.user);
   return (
     <>
       <Navbar className=" border-b-2 ">
@@ -18,48 +20,63 @@ export default function Header() {
           </span>
           Compass
         </Link>
-        <form >
-        <TextInput
+        <form>
+          <TextInput
             type="text"
             placeholder="Search..."
             rightIcon={AiOutlineSearch}
             className="hidden lg:inline "
-
-        />
-
+          />
         </form>
-        <Button className="w-12 h-10 lg:hidden  " color='gray' pill>
-          <AiOutlineSearch/>
+        <Button className="w-12 h-10 lg:hidden  " color="gray" pill>
+          <AiOutlineSearch />
         </Button>
         <div className="flex gap-2 md:order-2 ">
           <Button className="w-12 h-10 hidden sm:inline " color="gray" pill>
-            <FaMoon/>
+            <FaMoon />
           </Button>
-          <Link to='/sign-in' >
-            <Button className="" gradientDuoTone='purpleToBlue'  outline >
-              Sign in
-            </Button>
-          </Link>
-            <Navbar.Toggle></Navbar.Toggle>
+          {currentUser ? (
+            <Dropdown
+              arrowIcon={false}
+              inline
+              label={
+                <Avatar alt="user" img={currentUser.profilePicture}></Avatar>
+              }
+            >
+              <Dropdown.Header>
+                <span className="block text-sm">@{currentUser.username}</span>
+                <span className="block text-sm font-medium truncate">
+                  {currentUser.email}
+                </span>
+              </Dropdown.Header>
+              <Link to={"/dashboard?tab=profile"}>
+                <Dropdown.Item>Profile</Dropdown.Item>
+              </Link>
+              <Dropdown.Divider/>
+              <Dropdown.Item>
+                Sign Out
+              </Dropdown.Item>
+            </Dropdown>
+          ) : (
+            <Link to="/sign-in">
+              <Button className="" gradientDuoTone="purpleToBlue" outline>
+                Sign in
+              </Button>
+            </Link>
+          )}
+          <Navbar.Toggle />
         </div>
-          <Navbar.Collapse>
-            <Navbar.Link active={path==='/'} as={'div'} >
-              <Link to='/'>
-                Home
-              </Link>
-            </Navbar.Link>
-            <Navbar.Link active={path==='/about'} as={'div'} >
-              <Link to='/about'>
-                About
-              </Link>
-            </Navbar.Link>
-            <Navbar.Link active={path==='/projects'}as={'div'} >
-              <Link to='/projects'>
-                Projects
-              </Link>
-            </Navbar.Link>
-
-          </Navbar.Collapse>
+        <Navbar.Collapse>
+          <Navbar.Link active={path === "/"} as={"div"}>
+            <Link to="/">Home</Link>
+          </Navbar.Link>
+          <Navbar.Link active={path === "/about"} as={"div"}>
+            <Link to="/about">About</Link>
+          </Navbar.Link>
+          <Navbar.Link active={path === "/projects"} as={"div"}>
+            <Link to="/projects">Projects</Link>
+          </Navbar.Link>
+        </Navbar.Collapse>
       </Navbar>
     </>
   );
