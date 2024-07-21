78fdd66244be8967e84b5a6a09fdf3066b1acb4c
vishu bhandari <deevishu1234@gmail.com>
2024-07-21 15:10:36 +0530

    blog mern app

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -3,11 +3,13 @@ import { Link, useLocation } from "react-router-dom";
 import React from "react";
 import { AiOutlineSearch } from "react-icons/ai";
 import { FaMoon } from "react-icons/fa";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
+import { toggleTheme } from "../redux/theme/themeSlice";
 
 export default function Header() {
   const path = useLocation().pathname;
   const { currentUser } = useSelector((state) => state.user);
+  const dispatch = useDispatch();
   return (
     <>
       <Navbar className=" border-b-2 ">
@@ -32,7 +34,12 @@ export default function Header() {
           <AiOutlineSearch />
         </Button>
         <div className="flex gap-2 md:order-2 ">
-          <Button className="w-12 h-10 hidden sm:inline " color="gray" pill>
+          <Button
+            className="w-12 h-10 hidden sm:inline "
+            color="gray"
+            pill
+            onClick={() => dispatch(toggleTheme())}
+          >
             <FaMoon />
           </Button>
           {currentUser ? (
