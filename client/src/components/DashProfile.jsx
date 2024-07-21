d79fc2e247ef72368da806bacb5ce2dbe86677cb
vishu bhandari <deevishu1234@gmail.com>
2024-07-21 23:25:59 +0530

    profile page ui

--- client/src/components/DashProfile.jsx
+++ client/src/components/DashProfile.jsx
@@ -1,3 +1,4 @@
+import { Button, TextInput } from "flowbite-react";
 import React from "react";
 import { useSelector } from "react-redux";
 export default function DashProfile() {
@@ -5,11 +6,32 @@ export default function DashProfile() {
   return (
     <div className="max-w-lg mx-auto p-3 w-full">
       <h1 className="my-7 text-center font-semibold text-3xl ">Profile</h1>
-      <form className="flex flex-col ">
-      <div className="w-32 h-32 self-center cursor-pointer shadow-md overflow-hidden rounded-full ">
-
-        <img src={currentUser.profilePicture} alt="user" className="rounded-full w-full h-full border-8 object-cover border-[lightgray] " />
-      </div>
+      <form className="flex flex-col gap-4  ">
+        <div className="w-32 h-32 self-center cursor-pointer shadow-md overflow-hidden rounded-full ">
+          <img
+            src={currentUser.profilePicture}
+            alt="user"
+            className="rounded-full w-full h-full border-8 object-cover border-[lightgray] "
+          />
+        </div>
+        <TextInput
+          type="text"
+          id="username"
+          placeholder="Username"
+          defaultValue={currentUser.username}
+        ></TextInput>
+        <TextInput
+          type="email"
+          id="email"
+          placeholder="Email"
+          defaultValue={currentUser.email}
+        ></TextInput>
+        <TextInput
+          type="password"
+          id="password"
+          placeholder="Password"
+        ></TextInput>
+        <Button type="Submit">Update</Button>
       </form>
     </div>
   );
