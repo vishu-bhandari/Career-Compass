77b8faf1ec054d4ef0afaefc57f4651eeb2ce2be
vishu bhandari <deevishu1234@gmail.com>
2024-07-22 16:12:01 +0530

    user image update functionality

--- client/src/components/DashProfile.jsx
+++ client/src/components/DashProfile.jsx
@@ -2,15 +2,18 @@ import { Button, TextInput } from "flowbite-react";
 import React, { useState } from "react";
 import { useSelector } from "react-redux";
 
-
-
 export default function DashProfile() {
   const { currentUser } = useSelector((state) => state.user);
   const [imagefile, setImageFile] = useState(null);
-  const handleImageChange=(e)=>{
-    setImageFile(e.target.files[0]);
+  const [imageFileUrl, setImageFileUrl] = useState(null);
+  const handleImageChange = (e) => {
+    const file = e.target.files[0];
+    if (file) {
+      setImageFile(file);
+      setImageFileUrl(URL.createObjectURL(file));
+    }
   };
-  console.log(imagefile);
+  console.log(imagefile, imageFileUrl);
   return (
     <div className="max-w-lg mx-auto p-3 w-full">
       <h1 className="my-7 text-center font-semibold text-3xl ">Profile</h1>
@@ -18,7 +21,7 @@ export default function DashProfile() {
         <input type="file" accept="image/*" onChange={handleImageChange} />
         <div className="w-32 h-32 self-center cursor-pointer shadow-md overflow-hidden rounded-full ">
           <img
-            src={currentUser.profilePicture}
+            src={imageFileUrl || currentUser.profilePicture}
             alt="user"
             className="rounded-full w-full h-full border-8 object-cover border-[lightgray] "
           />
