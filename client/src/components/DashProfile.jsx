accb6660510c6f35e801fe83aa678d52a50207da
vishu bhandari <deevishu1234@gmail.com>
2024-07-24 20:50:10 +0530

    Dashboard

--- client/src/components/DashProfile.jsx
+++ client/src/components/DashProfile.jsx
@@ -1,11 +1,12 @@
 import { Button, TextInput } from "flowbite-react";
-import React, { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { useSelector } from "react-redux";
 
 export default function DashProfile() {
   const { currentUser } = useSelector((state) => state.user);
   const [imagefile, setImageFile] = useState(null);
   const [imageFileUrl, setImageFileUrl] = useState(null);
+  const filePickerRef = useRef();
   const handleImageChange = (e) => {
     const file = e.target.files[0];
     if (file) {
@@ -13,13 +14,35 @@ export default function DashProfile() {
       setImageFileUrl(URL.createObjectURL(file));
     }
   };
-  console.log(imagefile, imageFileUrl);
+
+  useEffect(()=>{
+    if(imagefile){
+      uploadImage();
+    }
+  },[imagefile]);
+
+  const uploadImage=async()=>{
+    console.log('uploading image...')
+  }
+
+
   return (
     <div className="max-w-lg mx-auto p-3 w-full">
       <h1 className="my-7 text-center font-semibold text-3xl ">Profile</h1>
       <form className="flex flex-col gap-4  ">
-        <input type="file" accept="image/*" onChange={handleImageChange} />
-        <div className="w-32 h-32 self-center cursor-pointer shadow-md overflow-hidden rounded-full ">
+        <input
+          className="hidden"
+          type="file"
+          accept="image/*"
+          onChange={handleImageChange}
+          ref={filePickerRef}
+        />
+        <div
+          className="w-32 h-32 self-center cursor-pointer shadow-md overflow-hidden rounded-full  "
+          onClick={() => {
+            filePickerRef.current.click();
+          }}
+        >
           <img
             src={imageFileUrl || currentUser.profilePicture}
             alt="user"
