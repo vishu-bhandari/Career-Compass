55d73e39ce06aba36bb4ee949656e143bb944d27
vishu bhandari <deevishu1234@gmail.com>
2024-07-26 15:58:18 +0530

    firebase storage

--- client/src/components/DashProfile.jsx
+++ client/src/components/DashProfile.jsx
@@ -1,11 +1,20 @@
 import { Button, TextInput } from "flowbite-react";
 import React, { useEffect, useRef, useState } from "react";
 import { useSelector } from "react-redux";
+import {
+  getDownloadURL,
+  getStorage,
+  ref,
+  uploadBytesResumable,
+} from "firebase/storage";
+import { app } from "../firebase";
 
 export default function DashProfile() {
   const { currentUser } = useSelector((state) => state.user);
   const [imagefile, setImageFile] = useState(null);
   const [imageFileUrl, setImageFileUrl] = useState(null);
+  const [imageFileUploadProgress, setFileUploadProgress] = useState(0);
+  const [imageFileUploadError, setImageFileUploadError] = useState(null);
   const filePickerRef = useRef();
   const handleImageChange = (e) => {
     const file = e.target.files[0];
@@ -22,7 +31,30 @@ export default function DashProfile() {
   }, [imagefile]);
 
   const uploadImage = async () => {
-    console.log("uploading image...");
+    const storage = getStorage(app);
+    const fileName = new Date().getTime() + imagefile.name;
+    const storageRef = ref(storage, fileName);
+    const uploadTask = uploadBytesResumable(storageRef, imagefile);
+    uploadTask.on(
+      "state_changed",
+      (snapshot) => {
+        const progress =
+          (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        //10.4222
+        setFileUploadProgress(progress.toFixed(0));
+        //10
+      },
+      (error) => {
+        setImageFileUploadError(
+          "Could not upload image(File must be less than 2MB)"
+        );
+      },
+      () => {
+        getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+          setImageFileUrl(downloadURL);
+        });
+      }
+    );
   };
 
   return (
