dbd3e41a9073e5287c9b60e965ba67c2e50d1fcd
vishu bhandari <deevishu1234@gmail.com>
2024-07-28 14:29:23 +0530

    user update api route

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,7 +1,27 @@
+import { errorHandler } from "../utils/error.js"
+import bcryptjs from 'bcryptjs'
+
 export const test = (req,res)=>{
     res.json({message:'api is working '})
 }
 
 export const updateUser=async (req,res, next)=>{
-    
-}
+   if(req.user.id !==req.params.userId){
+    return next(errorHandler(403,'You are not allowed to update this user'))
+   }
+   if(req.body.password){
+    if(req.body.password.length < 6){
+        return next(errorHandler(400, 'Password must be at least 6 characters'));
+
+    }
+    req.body.password = bcryptjs.hashSync(req.body.password, 10);
+   }
+   if(req.body.username){
+    if(req.body.username.length < 7 || req.body.username > 20){
+        return next(errorHandler(400, 'Username must be betweeen 7 and 20 characters'));
+    }
+   }
+   if(req.body.username.includes(' ')){
+    return next(errorHandler(400, 'Username cannot contain spaces'))
+   }
+};
--- api/utils/verifyUser.js
+++ api/utils/verifyUser.js
@@ -1,6 +1,18 @@
 import jwt from "jsonwebtoken";
-import { errorHandler } from "./error";
+import { errorHandler } from "./error.js";
 
 export const verifyToken = (req, res, next) => {
-  const token = req.cookie.access_token;
+  const token = req.cookies.access_token; // Corrected to `req.cookies`
+  
+  if (!token) {
+    return next(errorHandler(401, 'Unauthorized'));
+  }
+
+  jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+    if (err) {
+      return next(errorHandler(401, 'Unauthorized'));
+    }
+    req.user = user;
+    next();
+  });
 };
