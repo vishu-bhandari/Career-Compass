23bb8d2d22c84b72b36ac6fb9b0b4b2a2db4ea0a
vishu bhandari <deevishu1234@gmail.com>
2024-07-31 10:31:38 +0530

    admin functionality added

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -53,7 +53,7 @@ export const signin = async (req, res, next) => {
 
     const token = jwt.sign(
       {
-        id: validUser._id,
+        id: validUser._id, isAdmin: validUser.isAdmin
       },
       process.env.JWT_SECRET
     );
@@ -78,7 +78,7 @@ export const google = async (req, res, next) => {
     if (user) {
       const token = jwt.sign(
         {
-          id: user._id,
+          id: user._id, isAdmin: user.isAdmin
         },
         process.env.JWT_SECRET
       );
@@ -106,7 +106,7 @@ export const google = async (req, res, next) => {
 
       const token = jwt.sign(
         {
-          id: newUser._id,
+          id: newUser._id, isAdmin: newUser.isAdmin
         },
         process.env.JWT_SECRET
       );
--- api/models/user.model.js
+++ api/models/user.model.js
@@ -1,30 +1,34 @@
 import mongoose from "mongoose";
 
-const userSchema= new mongoose.Schema({
-    username:{
-        type:String,
-        required: true,
-        unique:true,
-
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
     },
-    email:{
-        type:String,
-        required:true,
-        unique:true,
-
+    email: {
+      type: String,
+      required: true,
+      unique: true,
     },
-    password:{
-        type:String,
-        required:true,
+    password: {
+      type: String,
+      required: true,
     },
-    profilePicture:{
-        type:String,
-        default:"https://images.pexels.com/photos/771742/pexels-photo-771742.jpeg?auto=compress&cs=tinysrgb&w=800",
-    }
+    profilePicture: {
+      type: String,
+      default:
+        "https://images.pexels.com/photos/771742/pexels-photo-771742.jpeg?auto=compress&cs=tinysrgb&w=800",
+    },
+    isAdmin: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  { timestamps: true }
+);
 
-}, {timestamps:true})
-
-
-const User=mongoose.model('User', userSchema);
+const User = mongoose.model("User", userSchema);
 
 export default User;
