f14ea46806a5bc37b7d06233c2d28bb9835d55a4
vishu bhandari <deevishu1234@gmail.com>
2024-08-02 12:22:48 +0530

    create get post api route

--- api/controllers/post.controller.js
+++ api/controllers/post.controller.js
@@ -2,7 +2,6 @@ import Post from "../models/post.model.js";
 import { errorHandler } from "../utils/error.js";
 
 export const create = async (req, res, next) => {
-   
   // Check if the user is an admin
   if (!req.user.isAdmin) {
     return next(errorHandler(403, "You are not allowed to create a post"));
@@ -29,6 +28,57 @@ export const create = async (req, res, next) => {
   try {
     const savedPost = await newPost.save();
     res.status(200).json(savedPost);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const getposts = async (req, res, next) => {
+  try {
+    const startIndex = parseInt(req.query.startIndex || 0);
+    const limit = parseInt(req.query.limit || 9);
+    const sortDirection = req.query.order === "asc" ? 1 : -1;
+
+    // Build query object based on query parameters
+    const query = {
+      ...(req.query.userId && { userId: req.query.userId }),
+      ...(req.query.category && { category: req.query.category }),
+      ...(req.query.slug && { slug: req.query.slug }), // Corrected the slug filter
+      ...(req.query.postId && { _id: req.query.postId }),
+      ...(req.query.searchTerm && {
+        $or: [
+          { title: { $regex: req.query.searchTerm, $options: "i" } },
+          { content: { $regex: req.query.searchTerm, $options: "i" } },
+        ],
+      })
+    };
+
+    // Fetch posts based on query
+    const posts = await Post.find(query)
+      .sort({ updatedAt: sortDirection })
+      .skip(startIndex)
+      .limit(limit);
+
+    // Count total posts with the same filters
+    const totalPosts = await Post.countDocuments(query);
+
+    // Calculate date for the start of the last month
+    const now = new Date();
+    const firstDayOfCurrentMonth = new Date(now.getFullYear(), now.getMonth(), 1);
+    const lastDayOfLastMonth = new Date(firstDayOfCurrentMonth - 1);
+
+    // Count posts created in the last month
+    const lastMonthPosts = await Post.countDocuments({
+      createdAt: { $gte: lastDayOfLastMonth }
+    });
+
+    // Respond with the results
+    res.status(200).json({
+      posts,
+      totalPosts,
+      lastMonthPosts
+    });
+
   } catch (error) {
     next(error);
   }
