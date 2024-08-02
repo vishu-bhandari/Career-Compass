10ca8bd9db238cf9648bdb60aa579533e6355c2c
vishu bhandari <deevishu1234@gmail.com>
2024-08-02 14:45:23 +0530

    Fetching post in the Dashboard posts page

--- client/src/components/DashPosts.jsx
+++ client/src/components/DashPosts.jsx
@@ -1,10 +1,28 @@
-import React from 'react'
+import { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
 
 export default function DashPosts() {
-  return (
-    <>
-    <div> Post</div>
+  const { currentUser } = useSelector((state) => state.user);
+  const [userPosts, setUserPosts] = useState([]);
+  console.log(userPosts);
+  useEffect(() => {
+    const fetchPosts = async () => {
+      try {
+        const res = await fetch(`/api/post/getposts?userId=${currentUser._id}`);
+        const data = await res.json();
+        if (res.ok) {
+          setUserPosts(data.posts);
+        }
+      } catch (error) {
+        console.log(error.message);
+      }
+    };
+    if (currentUser.isAdmin) {
+      fetchPosts();
+    }
+  }, [currentUser._id]);
 
-    </>
-  )
+  return (
+    <div ></div>
+  );
 }
