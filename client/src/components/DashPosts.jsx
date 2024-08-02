0d23775fc8cefec45152144fa94a7681ec498ad4
vishu bhandari <deevishu1234@gmail.com>
2024-08-02 21:11:01 +0530

    show user post inside dashboard

--- client/src/components/DashPosts.jsx
+++ client/src/components/DashPosts.jsx
@@ -1,10 +1,12 @@
+import { Table, TableCell, TableHead, TableHeadCell } from "flowbite-react";
 import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
 
 export default function DashPosts() {
   const { currentUser } = useSelector((state) => state.user);
   const [userPosts, setUserPosts] = useState([]);
-  console.log(userPosts);
+
   useEffect(() => {
     const fetchPosts = async () => {
       try {
@@ -23,6 +25,65 @@ export default function DashPosts() {
   }, [currentUser._id]);
 
   return (
-    <div ></div>
+    <div className=" table-auto overflow-x-auto md:mx-auto p-3 scrollbar scrollbar-track-slate-100  scrollbar-thumb-slate-300 dark:srollbar-thumb-slate-500  ">
+      {currentUser.isAdmin && userPosts.length > 0 ? (
+        <>
+          <Table hoverable className=" shadow-md">
+            <TableHead>
+              <TableHeadCell>Date Updated</TableHeadCell>
+              <TableHeadCell>Post Image</TableHeadCell>
+              <TableHeadCell>Post Title</TableHeadCell>
+              <TableHeadCell>Category</TableHeadCell>
+              <TableHeadCell>Delete</TableHeadCell>
+              <TableHeadCell>
+                <span>Edit</span>
+              </TableHeadCell>
+            </TableHead>
+            {userPosts.map((post) => (
+              <Table.Body className=" divide-y">
+                <Table.Row className=" bg-white dark:border-gray-700 dark:bg-gray-800">
+                  <TableCell>
+                    {new Date(post.updatedAt).toLocaleDateString()}
+                  </TableCell>
+                  <TableCell>
+                    <Link to={`/post/${post.slug}`}>
+                      <img
+                        src={post.image}
+                        className="w-20 h-10 object-cover bg-gray-500 rounded"
+                        alt={post.title}
+                      />
+                    </Link>
+                  </TableCell>
+                  <TableCell>
+                    <Link
+                      className=" font-medium text-gray-800 dark:text-white"
+                      to={`/post/${post.slug}`}
+                    >
+                      {post.title}
+                    </Link>
+                  </TableCell>
+                  <TableCell>{post.category}</TableCell>
+                  <TableCell>
+                    <span className=" font-medium text-red-500 hover:underline cursor-pointer">
+                      Delete
+                    </span>
+                  </TableCell>
+                  <TableCell>
+                    <Link
+                      className="text-teal-500  hover:underline"
+                      to={`/update-post/${post._id}`}
+                    >
+                      <span>Edit</span>
+                    </Link>
+                  </TableCell>
+                </Table.Row>
+              </Table.Body>
+            ))}
+          </Table>
+        </>
+      ) : (
+        <p>You have no Posts yet!</p>
+      )}
+    </div>
   );
 }
