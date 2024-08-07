193e512b316d196de1592befb329a9ee25bef6ba
vishu bhandari <deevishu1234@gmail.com>
2024-08-08 01:28:17 +0530

    pre installation added

--- api/index.js
+++ api/index.js
@@ -1,11 +1,12 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
-import userRoutes from './routes/user.route.js';
-import authRoutes from './routes/auth.route.js';
-import postRoutes from './routes/post.route.js'
+import userRoutes from "./routes/user.route.js";
+import authRoutes from "./routes/auth.route.js";
+import postRoutes from "./routes/post.route.js";
 import cookieParser from "cookie-parser";
-import commentRoutes from './routes/comment.route.js'
+import commentRoutes from "./routes/comment.route.js";
+import path from "path";
 
 dotenv.config();
 
@@ -14,6 +15,7 @@ mongoose
   .then(() => console.log("Connected to MongoDB"))
   .catch((err) => console.error("Could not connect to MongoDB", err));
 
+const __dirname = path.resolve();
 const app = express();
 
 app.use(express.json());
@@ -24,21 +26,23 @@ app.listen(3000, () => {
   console.log("server is running on port 3000");
 });
 
+app.use("/api/user", userRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/post", postRoutes);
+app.use("/api/comment", commentRoutes);
 
-app.use('/api/user',userRoutes);
-app.use('/api/auth', authRoutes);
-app.use('/api/post' , postRoutes );
-app.use('/api/comment' , commentRoutes);
+app.use(express.static(path.join(__dirname, "/client/dist")));
 
-
-app.use((err,req,res,next)=>{
-  const statusCode=err.statusCode || 500;
-  const message=err.message || 'Internal Server Error';
-  res.status(statusCode).json({
-    success:false,
-    statusCode,
-    message
-  });
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
 });
 
-
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Server Error";
+  res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
+});
